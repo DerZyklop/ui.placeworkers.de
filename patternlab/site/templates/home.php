2f5181bbdf837a15a40daeb4ae2de70b955ba83e
Bastian Allgeier <mail@bastianallgeier.com>
2014-07-03 13:15:22 +0200

    new homepage

--- patternlab/site/templates/home.php
+++ patternlab/site/templates/home.php
@@ -1,11 +1,28 @@
 <?php snippet('header') ?>
 
-<?php if(param('format') == 'iframe'): ?>
-  <?php foreach($pages->find('atoms', 'molecules', 'organisms')->children()->children() as $pattern): ?>
-  <?php snippet('listitem', array('pattern' => $pattern)) ?>
-  <?php endforeach ?>
-<?php else: ?>
-  <iframe class="pl-iframe" src="<?php echo $page->url() ?>/format:iframe"></iframe>
-<?php endif ?>
+<div class="pl-content">
+
+  <h2 class="pl-pattern-headline">
+    <a href="#"><?php echo html($page->headline()) ?></a>
+  </h2>
+
+  <?php echo kirbytext($page->text()) ?>
+
+  <h2 id="downloads" class="pl-pattern-headline">
+    <a href="#downloads">Downloads</a>
+  </h2>
+
+  <ul>
+    <li><a href="<?php echo url('styles.css') ?>">styles.css</a></li>
+    <li><a href="<?php echo url('styles.min.css') ?>">styles.min.css</a></li>
+  </ul>
+
+  <h2 id="repository" class="pl-pattern-headline">
+    <a href="#repository">Repository</a>
+  </h2>
+
+  <p><a href="<?php echo $page->repository() ?>"><?php echo $page->repository() ?></a></p>
+
+</div>
 
 <?php snippet('footer') ?>
