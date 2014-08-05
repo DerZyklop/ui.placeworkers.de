741643fd425fb1529c9fa59ba4d2a0cfed9bdb36
DerZyklop <mail@der-zyklop.de>
2014-08-05 19:38:43 +0200

    new content

---
+++ content/2-atoms/3-forms/1-inputs-disabled/template.php
@@ -0,0 +1,27 @@
+<div class="field">
+  <input class="input" id="text" type="text" placeholder="Text" disabled="disabled">
+</div>
+
+<div class="field">
+  <input class="input" id="password" type="password" placeholder="Password" disabled="disabled">
+</div>
+
+<div class="field">
+  <input class="input" id="url" type="url" placeholder="http://yoursite.com" disabled="disabled">
+</div>
+
+<div class="field">
+  <input class="input" id="email" type="email" placeholder="name@email.com" disabled="disabled">
+</div>
+
+<div class="field">
+  <input class="input" id="text" type="tel" placeholder="+49 800 123 456" disabled="disabled">
+</div>
+
+<div class="field">
+  <input class="input" id="text" type="number" placeholder="12" pattern="[0-9]*" disabled="disabled">
+</div>
+
+<div class="field">
+  <textarea class="input" id="textarea" rows="8" cols="48" placeholder="Text" disabled="disabled"></textarea>
+</div>
